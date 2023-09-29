f4729ba7fdbd1f22780ab52af3b9db854792072e
krrattoss5 <krrattoss5@gmail.com>
2023-09-28 22:35:42 -0500

    (add/localStorage)

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,7 +10,7 @@ import { useEffect } from 'react';
 
 export default function Card(props) {
 
-  const { car } = useSelector(s=>s)
+  const car = useSelector(s=>s.car)
   const dispatch = useDispatch()
   const [inCar,setInCar] = useState(false)
 
